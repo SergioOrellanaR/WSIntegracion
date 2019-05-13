f791b26ff9fd4412de3d7fbc0c337473e0572512
DESKTOP-72RTSVI\Brayan <brayan.mendez@taisachile.cl>
2019-05-13 13:54:52 -0400

    Implementacion google maps API geocode

---
+++ Lumen Services/DabbawallasServiceLumen/app/Http/Controllers/GeorefController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DateTime;
+use Illuminate\Http\Request;
+
+class GeorefController extends Controller
+{
+    function geocodeResponse (Request $request){
+
+        function geocode($address)
+        {
+            $address = urlencode($address);            
+            $url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key=AIzaSyC3IIXSR89fmcEcNnODeKzBvwhfQb_LFCg";
+            $resp_json = file_get_contents($url);
+            $resp = json_decode($resp_json, true);
+            if (empty($resp)) {
+                return false;
+            } else {
+                if ($resp['status'] == 'OK') {
+                    $tipo = $resp['results'][0]['types'][0];
+                    if ($tipo == "street_address") {
+                        $location = $resp['results'][0]['geometry']['location'];
+                        return $location;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        
+            if ($request->isJson())
+            {
+                $obJson = json_decode($request->getContent());
+                $calle= $obJson->calle;
+                $comuna= $obJson->comuna;
+                $ciudad= $obJson->ciudad;
+                $direccion= $calle . ' ' . ' ' . $comuna . ' ' . $ciudad . ' Chile';
+                $resp_geo = geocode($direccion);
+                $latitud = $resp_geo['lat'];
+                $longitud = $resp_geo['lng'];
+
+                $array = [
+                    'success'=> true,
+                    'latitud' => $latitud,
+                    'longitud' => $longitud,
+                ];
+
+                if(!$resp_geo){
+                    return response()->json(['success' => false, 'error' => 'Dirección no encontrada'], 500);
+                }else{
+                    return  response()->json($array,200);
+                }
+
+
+            } else {
+                return response()->json(['success' => false, 'error' => 'Debe ingresar un Json'], 400, []);
+            }
+
+        }
+}
